6961fc48543d9015a75a97a1ff993cd9c38b2f56
Yusufola157 <yusufolaseni602@gmail.com>
2023-07-07 17:34:08 +0100

    chupdtd

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,13 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../page/Home/Main";
 import Level14 from "../page/Home/Level14";
-
+import Level5 from "../page/Home/Level5";
 export const routes = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
   },
 
+  {
+    path:"/level5",
+    element: <Level5 />,
+  },
   {
     path:"/level14",
     element: <Level14 />,
